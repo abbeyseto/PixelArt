06cf612e7467c92c0f30a09a3af47608fddf9bb2
Adenle Abiodun <adenleabbey@gmail.com>
2018-06-24 23:27:31 +0100

    comments added to the .js file
    Comment has been added to this JavaScript file and the function to color grid the grid has been modified to enable users add a color to the grid and remove the color on second click (which ever comes first).

--- designs.js
+++ designs.js
@@ -1,55 +1,45 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
 // FUNCION TO MAKE GRID
-function makeGrid() {
+function makeGrid() { // Begining of the function that executes to draw the grid on the document.
 
-  var heigth = parseInt($('#inputHeight').val());
-  var width = parseInt($('#inputWeight').val());
+  var heigth = $('#inputHeight').val(); //capture the height inputed by the user.
+  var width = $('#inputWeight').val(); // capture the width inputed by the user.
 
-  for (var col = 0; col < heigth; col++) {
-    $("#pixelCanvas").append("<tr></tr>");
+  for (var col = 0; col < heigth; col++) { // for loop that forms the grid
+    $("#pixelCanvas").append("<tr></tr>"); // appends multiple <tr> to pixelCanvas depending on the value of heigth
     for (var row = 0; row < width; row++) {
-      $("tr").last().append("<td></td>");
+      $("tr").last().append("<td></td>"); // appends multiple <td> to <tr> element depending on the value of width
     }
   }
 
-};
+}; // End of the function that executes to draw the grid on the document.
 
-/*$('#colorPicker').on ("hover", function(){
-  $('#colorPicker').attr('title', 'Click here  to select a color')};
-*/
-$("#colorPicker").hover(function () {
-  $(this).css('cursor', 'pointer').attr('title', 'Hey dude! ... Pick a color of your choice');
+// COLOR PICKER EVENTS
+$("#colorPicker").hover(function () { //on hover function on the color picker element
+  $(this).css('cursor', 'pointer').attr('title', 'Hey dude! ... Pick a color of your choice'); // when mouse hovers on the color picker element, it shows the value of "attr"
 }, function () {
-  $(this).css('cursor', 'auto');
+  $(this).css('cursor', 'auto'); // specified the type of cursor displayed when hovering on the colorpicker element
 });
 
 // Function to clear the GRID
-function clearGrid() {
+function clearGrid() {  // this function resets the grid and all the colors that have been applied when called
   $("#pixelCanvas").children().remove();
 };
 
 //EVENT LISTENER TO CREATE THE GRID
-$('#sizePicker').children().last().click(function (event) {
+$('#sizePicker').children().last().click(function (event) {   // click event when the submit button is pressed. children().last() refers to the last element in the #sizePicker(submit button)
   event.preventDefault(); // to avoid page reload
-  clearGrid();
-  makeGrid();
+  clearGrid();    // each time submit is clicked, this called function clears the grid first
+  makeGrid();     // after grid is cleared, this called function makes a new grid
 });
 
 
 //EVENT LISTENER AND FUNCION TO COLOR THE GRID
-$("#pixelCanvas").on("click", "td", function () {
-  var color = $('#colorPicker').val();
-  $(this).css("backgroundColor", color);
-});
-
-//EVENT LISTENER AND FUNCION TO RESET THE COLOR TO DEFAULT
-$("#pixelCanvas").on("dblclick", "td", function (e) {
-  //$(this).addClass('default');
-  defaultColor = $('body').css('backgroundColor');
-  $(this).css('backgroundColor', defaultColor);
-  e.preventDefault();
+$("#pixelCanvas").on('click', "td", function () {  // when <td> element is clicked
+  var color = $('#colorPicker').val();              //color is already set to the current color at the color picker element
+  if ($(this).attr('style')) {                      // if this <td> element already has a color applied to it,
+    $(this).removeAttr('style');                     // remove the color and apply no other color
+  }
+  else {                                             // but if no color has been applied.
+    $(this).css('background', color);               // the current color at the color picker element is applied as a background color of the <td> element
+  }
 });
